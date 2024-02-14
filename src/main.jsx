69155744b34565627e63983fb855985a0892efcd
murilo <mhenrique.mb@gmail.com>
2024-02-14 11:18:01 -0300

    Adding global.js styles file

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+
+import GlobalStyles from './styles/global.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles></GlobalStyles>
+
+    </ThemeProvider>
   </React.StrictMode>,
 )
