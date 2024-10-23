d319826050febf21c2dfe823d9a37d1fce7a70c0
murilo <mhenrique.mb@gmail.com>
2024-10-23 11:56:02 -0300

    adjusting and integrate to the database

--- src/components/OrderItem/index.jsx
+++ src/components/OrderItem/index.jsx
@@ -2,22 +2,53 @@ import { Container } from "./styles";
 import { useAuth } from "../../hooks/auth.jsx";
 import { USER_ROLE } from '../../utils/roles.js';
 
-export function OrderItem (){
+export function OrderItem (data, ...rest){
   const { user } = useAuth();
+  const orderData = data.data
+  const orderId = `${orderData.id}`
+  const orderDate = `${orderData.created_at}`
+  const orderStatus = `${orderData.status}`
+  const orderDetails = `${orderData.details}`
+
+  function translateOrderStatus(status){
+    switch(status) {
+      case 'pending':
+        return 'Pendente'
+      case 'preparing':
+        return 'Preparando'
+      case 'delivered':
+        return 'Entregue'
+      default:
+        return 'Pendente'
+    }
+  }
+
+  function colorOrderStatus(status){
+    switch(status) {
+      case 'pending':
+        return "#AB222E"
+      case 'preparing':
+        return '#FBA94C'
+      case 'delivered':
+        return '#04D361'
+      default:
+        return '#AB222E'
+    }
+  }
 
   return (
     <Container>
       <head>
-        <div className="order-code">000004</div>
+        <div className="order-code">{orderId}</div>
         <div className="order-status">
           <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="4" cy="4" r="4" transform="matrix(1 0 0 -1 0 8)" fill="#AB222E"/>
+            <circle cx="4" cy="4" r="4" transform="matrix(1 0 0 -1 0 8)" fill={colorOrderStatus(orderStatus)}/>
           </svg>
-          Pedente
+          {translateOrderStatus(orderStatus)}
         </div>
-        <div className="order-date">20/05 às 18h00</div>
+        <div className="order-date">{orderDate}</div>
       </head>
-      <div className="order-description">1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</div>
+      <div className="order-description">{orderDetails}</div>
 
       <div className="select-order-status">
         {
