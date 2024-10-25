1ef87e858d5ef9dee4f9973afaf3bd0e3f4aab49
murilo <mhenrique.mb@gmail.com>
2024-10-25 11:01:05 -0300

    updating baseURL to production environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://explorer-api-90x6.onrender.com",
   withCredentials: true
 });
