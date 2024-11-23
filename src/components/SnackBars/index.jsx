63386aa35d85028f9fcba5e2f9f6b42ec6ce87e5
murilo <mhenrique.mb@gmail.com>
2024-11-23 13:37:36 -0300

    creating snackbars for alert messages

---
+++ src/components/SnackBars/index.jsx
@@ -0,0 +1,23 @@
+import { Snackbar, Alert } from "@mui/material";
+import { useState } from 'react';
+
+export function Snackbars({ title, severity, autoHideDuration = 6000, onClose, ...rest }) {
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") return;
+    if (onClose) onClose(event);
+  };
+
+  return(
+    <Snackbar
+      autoHideDuration={autoHideDuration}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      {...rest}
+    >
+      <Alert onClose={handleClose} severity={severity} variant="filled">
+        {title}
+      </Alert>
+    </Snackbar>
+  )
+}
