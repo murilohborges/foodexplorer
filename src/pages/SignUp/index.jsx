775a5381ac24dba8253dd866a4f95e9fc7c9cfa1
murilo <mhenrique.mb@gmail.com>
2024-11-23 17:06:04 -0300

    correcting path of SnackBars Component

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,7 +1,7 @@
 import { Container, Form, HeaderLogo } from "./styles.js";
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { Snackbars } from '../../components/Snackbars/index.jsx';
+import { Snackbars } from '../../components/Snackbars';
 import { Link, useNavigate } from "react-router-dom";
 import{ useState, useEffect } from 'react';
 import { api } from '../../services/api.js'
