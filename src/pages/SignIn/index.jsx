280bdc464a10e82b309a26f1dba50c3471e8f913
murilo <mhenrique.mb@gmail.com>
2024-11-30 16:46:04 -0300

    editing snackbars context

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,41 +1,21 @@
 import { Container, Form, HeaderLogo } from "./styles.js";
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { Snackbars } from '../../components/Snackbar';
 import { Link } from "react-router-dom";
 import { useAuth } from '../../hooks/auth';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSnackbar } from '../../context/SnackbarContext.jsx';
 
 export function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [openSnackbar, setOpenSnackbar] = useState(false);
   const { signIn } = useAuth();
-  const { snackbarMessage, severity, updateSnackbarMessage, clearSnackbarMessage } = useSnackbar();
-
-  useEffect(() => {
-    if (snackbarMessage) {
-      setOpenSnackbar(true);
-    } else if (loading) {
-      setOpenSnackbar(true);
-      updateSnackbarMessage("Carregando...", "info");
-    }
-  }, [snackbarMessage, loading]);
+  const { updateSnackbarMessage, clearSnackbarMessage } = useSnackbar();
   
   async function handleSignIn(){
-    setLoading(true);
+    updateSnackbarMessage("Carregando", "info");
     await signIn({ email, password });
-    setTimeout(() =>{
-      setLoading(false);
-    }, 1000)
   }
-
-  const handleCloseSnackbar = () => {
-    setOpenSnackbar(false);
-    clearSnackbarMessage();
-  };
 
   async function handleToRegister(){
     clearSnackbarMessage();
@@ -43,13 +23,6 @@ export function SignIn() {
 
   return(
     <Container>
-      <Snackbars 
-        open={openSnackbar}
-        severity={severity} 
-        title={snackbarMessage}
-        onClose={handleCloseSnackbar} 
-      />
-
       <HeaderLogo>
         <svg width="39" height="44" viewBox="0 0 39 44" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M19.6574 0L38.4133 10.8287V32.4862L19.6574 43.3149L0.901548 32.4862V10.8287L19.6574 0Z" fill="#065E7C"/>
