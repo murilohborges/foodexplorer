8d9410ce58e275a2eab16c104755dd659abd460f
murilo <mhenrique.mb@gmail.com>
2024-12-02 11:58:24 -0300

    adding favicon filled and empty and logic to show them

--- src/components/Plate/index.jsx
+++ src/components/Plate/index.jsx
@@ -4,7 +4,7 @@ import { api } from '../../services/api';
 import { USER_ROLE } from '../../utils/roles.js';
 import { useAuth } from "../../hooks/auth.jsx";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSnackbar } from '../../context/SnackbarContext.jsx';
 
 export function Plate({ data, receivedOrderToCart, ...rest }){
@@ -17,6 +17,7 @@ export function Plate({ data, receivedOrderToCart, ...rest }){
   const navigate = useNavigate();
   const avatarUrl = data.avatar ? `${api.defaults.baseURL}/files/${data.avatar}` : plateIcon;
   const [numberOrders, setNumberOrders] = useState(Number('1'));
+  const [isFavourited, setIsFavourited] = useState(false);
   const { updateSnackbarMessage } = useSnackbar();
 
   function handleDetails(id){
@@ -73,6 +74,25 @@ export function Plate({ data, receivedOrderToCart, ...rest }){
     }
   }
 
+  async function handleRemoveFav(){
+    await api.delete(`/favourites/${PlateId}`);
+    navigate("/");
+    updateSnackbarMessage("Prato removido dos favoritos!", "success")
+  }
+
+  useEffect(() => {
+    async function fetchPlates(){
+      const response = await api.get(`/favourites?title=`);
+      const dataPlate = response.data;
+      if(dataPlate.length != 0 && dataPlate[0].plate_id == PlateId){
+        setIsFavourited(true)
+      } else {
+        setIsFavourited(false)
+      }
+      
+    }
+    fetchPlates();
+  },[])
 
   return(
     <swiper-slide>
@@ -92,10 +112,12 @@ export function Plate({ data, receivedOrderToCart, ...rest }){
         {
           [USER_ROLE.CUSTOMER].includes(user.role) && 
           <>
-            <FavIcon onClick={handleFavPlate}>
-              <svg width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M13.6997 22.704L23.0754 12.478C25.3788 9.95304 25.7145 5.82477 23.5384 3.18621C22.9926 2.52129 22.3283 1.9847 21.586 1.60925C20.8437 1.23379 20.0391 1.02735 19.2211 1.00254C18.4032 0.977725 17.5893 1.13506 16.829 1.46493C16.0687 1.7948 15.3781 2.29028 14.7993 2.9211L13.0399 4.85267L11.5236 3.18621C9.2086 0.673905 5.4236 0.30779 3.00443 2.68123C2.39479 3.27646 1.90282 4.00102 1.55859 4.81063C1.21435 5.62024 1.02508 6.4979 1.00233 7.38999C0.979577 8.28208 1.12383 9.16986 1.42627 9.99908C1.72872 10.8283 2.183 11.5816 2.76136 12.2129L12.3801 22.704C12.5557 22.8936 12.7928 23 13.0399 23C13.287 23 13.5241 22.8936 13.6997 22.704Z" stroke="#E1E1E6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
+            <FavIcon>
+              {isFavourited ? <svg className="filled" onClick={handleRemoveFav} width="26" height="24" viewBox="0 0 26 24" fill="#750310" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.6997 22.704L23.0754 12.478C25.3788 9.95304 25.7145 5.82477 23.5384 3.18621C22.9926 2.52129 22.3283 1.9847 21.586 1.60925C20.8437 1.23379 20.0391 1.02735 19.2211 1.00254C18.4032 0.977725 17.5893 1.13506 16.829 1.46493C16.0687 1.7948 15.3781 2.29028 14.7993 2.9211L13.0399 4.85267L11.5236 3.18621C9.2086 0.673905 5.4236 0.30779 3.00443 2.68123C2.39479 3.27646 1.90282 4.00102 1.55859 4.81063C1.21435 5.62024 1.02508 6.4979 1.00233 7.38999C0.979577 8.28208 1.12383 9.16986 1.42627 9.99908C1.72872 10.8283 2.183 11.5816 2.76136 12.2129L12.3801 22.704C12.5557 22.8936 12.7928 23 13.0399 23C13.287 23 13.5241 22.8936 13.6997 22.704Z" stroke="#750310" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg> : <svg className="empty" onClick={handleFavPlate} width="26" height="24" viewBox="0 0 26 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.6997 22.704L23.0754 12.478C25.3788 9.95304 25.7145 5.82477 23.5384 3.18621C22.9926 2.52129 22.3283 1.9847 21.586 1.60925C20.8437 1.23379 20.0391 1.02735 19.2211 1.00254C18.4032 0.977725 17.5893 1.13506 16.829 1.46493C16.0687 1.7948 15.3781 2.29028 14.7993 2.9211L13.0399 4.85267L11.5236 3.18621C9.2086 0.673905 5.4236 0.30779 3.00443 2.68123C2.39479 3.27646 1.90282 4.00102 1.55859 4.81063C1.21435 5.62024 1.02508 6.4979 1.00233 7.38999C0.979577 8.28208 1.12383 9.16986 1.42627 9.99908C1.72872 10.8283 2.183 11.5816 2.76136 12.2129L12.3801 22.704C12.5557 22.8936 12.7928 23 13.0399 23C13.287 23 13.5241 22.8936 13.6997 22.704Z" stroke="#E1E1E6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg> }
             </FavIcon>
           </> 
         }
